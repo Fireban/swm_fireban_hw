65ae835ae5aa502c1d8484e6d1dd9bcdee939ece
Gseung <gseung96@gmail.com>
2020-12-14 13:49:47 +0000

    update init_sh

--- init_sh.sh
+++ init_sh.sh
@@ -8,12 +8,12 @@ echo Y | sudo apt-get install v4l2loopback-utils
 echo Y | sudo apt-get install raspberrypi-kernel-headers
 echo Y | sudo apt-get install libcurl4-openssl-dev
 
-sudo cp fireban-hw/thermal-imaging-camera/example/10-seekthermal.rules /etc/udev/rules.d/
+sudo cp ./thermal-imaging-camera/example/10-seekthermal.rules /etc/udev/rules.d/
 sudo udevadm control --reload
 
-sudo cp fireban-hw/thermal-imaging-camera/example/lib/libseekware.so* /usr/lib
+sudo cp ./thermal-imaging-camera/example/lib/libseekware.so* /usr/lib
 sudo mkdir /usr/include/seekware
-sudo cp fireban-hw/thermal-imaging-camera/example/include/seekware/seekware.h /usr/include/seekware
+sudo cp ./thermal-imaging-camera/example/include/seekware/seekware.h /usr/include/seekware
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt install git-lfs
