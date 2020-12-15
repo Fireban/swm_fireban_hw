048e7e1fd74069bd1a19c0bb9ed2118abfe8423f
Gseung <gseung96@gmail.com>
2020-12-15 07:52:55 +0000

    update save path & autorun

--- init_sh.sh
+++ init_sh.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo Y | sudo apt-get update
+sudo apt-get update
 echo Y | sudo apt-get upgrade
-echo Y | sudo apt-get install vim
 echo Y | sudo apt-get install libusb-1.0 libusb-dev libsdl2-dev
 echo Y | sudo apt-get install libusb-1.0-0-dev libusb-1.0-0
 echo Y | sudo apt-get install v4l2loopback-utils
@@ -15,11 +14,11 @@ sudo cp ./thermal-imaging-camera/example/lib/libseekware.so* /usr/lib
 sudo mkdir /usr/include/seekware
 sudo cp ./thermal-imaging-camera/example/include/seekware/seekware.h /usr/include/seekware
 
+sudo cp ./thermal-imaging-camera/example/src/seekware-filter/loop_exe.sh /home/pi
+
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt install git-lfs
 #cd fireban-hw && git lfs pull
 
-sudo depmod -a
-sudo modprobe v4l2loopback video_nr=4
 
 
--- thermal-imaging-camera/example/src/seekware-stream_v2/loop_exe.sh
+++ thermal-imaging-camera/example/src/seekware-stream_v2/loop_exe.sh
@@ -3,13 +3,23 @@
  while [ 1 ]
      do
          Cnt1=`ps -aux|grep "seekware-sdl"|grep -v grep|wc -l`
+         Cnt2=`ps -aux|grep "autorun"|grep -v grep|wc -l`
          PROCESS1=`ps -aux|grep "seekware-sdl"|grep -v grep|awk '{print $1}'`
+         PROCESS2=`ps -aux|grep "autorun"|grep -v grep|awk '{print $1}'`
      if [ $Cnt1 -ne 0 ]
          then
-             echo "seekware-sdl (PID : $PROCESS1) alredy runing"
+             echo "seekware-sdl (PID : $PROCESS1) already runing"
      else
-         ./swm_fireban-hw/thermal-imaging-camera/example/src/seekware-stream_v2//seekware-sdl &
+         ./seekware-sdl &
          echo "seekware-sdl run"
+     fi
+
+     if [ $Cnt2 -ne 0 ]
+         then
+             echo "autorun (PID : $PROCESS2) already runing"
+     else
+	 ./autorun &
+	 echo "autorun run"
      fi
      sleep 3
  done
